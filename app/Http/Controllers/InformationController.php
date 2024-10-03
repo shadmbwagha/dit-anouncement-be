5ed19188b4ae3aeeafd9f44dc424e4a43f3c7717
shad mbwagha <shadracksylvestar@gmail.com>
2024-10-03 07:15:24 +0300

    last apis

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Information;
 
+
 class InformationController extends Controller
 {
     // Store new information
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\InformationController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('information', InformationController::class);
